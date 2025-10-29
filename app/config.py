07984d25527bcf4c6903df706c39ae697869af34
Loran De Vriendt <loran.devriendt@UGent.be>
2025-10-29 14:37:13 +0100

    ready for launch

---
+++ app/config.py
@@ -0,0 +1,5 @@
+class ConfigABC:
+    SECRET_KEY = 'your_secret_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost:port/dbname'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
