07175dfefa49f8e0173f47e9e8b0e4458d9b26fd
VincentPardo <Vincent.Pardo@UGent.be>
2025-11-01 19:22:21 +0100

    test

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config:
-    SECRET_KEY = 'you-will-never-guess'
+    SECRET_KEY = 'you-will-never-guesssss'
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Iegfmm740_0@db.fsqylzxocszdduainvqd.supabase.co:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
