979a63bf5dd929dfa8ebd00fc2d3bbe5bbf31608
Loran De Vriendt <loran.devriendt@UGent.be>
2025-11-08 10:14:31 +0100

    basic tabel personen

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from app.models import Persoon
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return "Welkom bij je Flask-app met configuratie! 🎉"
+    return "Welkom bij je Flask-app met Supabase"
+
+@main.route('/personen')
+def personen_tabel():
+    kolomnamen = [kolom.name for kolom in Persoon.__table__.columns]
+    personen = []
+    return render_template("personen.html", kolomnamen=kolomnamen, personen=personen)
+
+
