b3ea9bf87b50cc7360cc07f86b08800209d2c287
Loran De Vriendt <loran.devriendt@UGent.be>
2025-11-11 21:20:25 +0100

    render

--- app/config.py
+++ app/config.py
@@ -1,6 +1,12 @@
+import os
+
 class Config:
     SECRET_KEY = 'you-will-never-guesssss'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Iegfmm740_0@db.fsqylzxocszdduainvqd.supabase.co:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL',
+        'postgresql://postgres:Iegfmm740_0@db.fsqylzxocszdduainvqd.supabase.co:5432/postgres'
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 
